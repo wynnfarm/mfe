939ceaab9290fa153c712ce608ae825266c166bf
wynnfarm <5656758+wynnfarm@users.noreply.github.com>
2024-05-10 10:23:51 -0500

    wip

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -75,7 +75,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             to={isSignedIn ? "/" : "/auth/signin"}
             onClick={onClick}
           >
-            {isSignedIn ? "Logout" : "Login"}
+            {isSignedIn ? "Logout!" : "Login!"}
           </Button>
         </Toolbar>
       </AppBar>
