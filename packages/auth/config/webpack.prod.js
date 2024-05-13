c55ddd57175b8a405c3f945f241e7ad7aa0a7b8f
wynnfarm <5656758+wynnfarm@users.noreply.github.com>
2024-05-13 09:06:40 -0500

    publicPath to auto

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/auth/latest/",
+    publicPath: "auto",
   },
   plugins: [
     new ModuleFederationPlugin({
